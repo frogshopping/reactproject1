a6d57ce44ceb965c219e02b39ca1c7daf30ee9a0
frogshopping <amirul.alam.chowdhuryy@gmail.com>
2024-02-19 19:12:11 +0600

    first solo commit

---
+++ src/components/todo.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+const [todo_task,settodo_task]=useState('')
+const Todo_component=()=>{
+    return(
+        <div>
+            <input></input>
+        </div>
+
+    )
+}
