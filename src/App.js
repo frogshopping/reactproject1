8ca01d846a07f784a16530d2ee5e5a5d3528bff9
frogshopping <amirul.alam.chowdhuryy@gmail.com>
2024-02-28 17:18:33 +0600

    todo list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import { Todo_Comp } from './components/todo';
 
 function App() {
   return (
     <div className="App">
-      
+      <Todo_Comp />
     </div>
   );
 }
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,11 +1,85 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const [todo_task,settodo_task]=useState('')
-const Todo_component=()=>{
-    return(
-        <div>
-            <input></input>
-        </div>
+export const Todo_Comp=()=>{
 
-    )
+  const [todo_list,settodo_list]=useState([])
+  const [new_task,setnew_task]=useState('')
+
+  const inp_tsk_fnc=(event)=>{
+    setnew_task(event.target.value)
+  }
+
+  const add_task_func=()=>{
+    const task={
+      id:todo_list.length===0? 1: todo_list[todo_list.length-1].id +1,
+      taskname: new_task
+    }
+    settodo_list([...todo_list, task])
+  }
+
+  const delete_task_func=(id)=>{
+    settodo_list(todo_list.filter((pointer)=> pointer.id!==id))
+  }
+
+
+  return (
+    <div>
+      <div className="tasks">
+        <input onChange={inp_tsk_fnc}></input>
+        <button onClick={add_task_func}>Add Task</button>
+      </div>
+
+      <div className="list">
+        {todo_list.map((task) => {
+          return (
+            <div>
+              <h1>{task.taskname}</h1>
+              <button onClick={()=> delete_task_func(task.id)}>x</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
+// export const Todo_component = () => {
+//   const [todo_list, settodo_list] = useState([]);
+//   const [new_task, setnew_task] = useState("");
+
+//   const inp_func = (event) => {
+//     setnew_task(event.target.value);
+//   };
+
+//   const add_task_func = () => {
+//     const task={
+//       id: todo_list.length===0? 1 : todo_list[todo_list.length-1].id+1,
+//       taskname: new_task
+//     }
+//     settodo_list([...todo_list, task]);
+//   };
+
+//   const delete_task_func =(id)=>{
+//     settodo_list(todo_list.filter((task)=> task.id!==id))
+//   }
+
+//   return (
+//     <div>
+//       <div className="Addtask">
+//         <input onChange={inp_func}></input>
+//         <button onClick={add_task_func}>Add Task</button>
+//       </div>
+//       <div className="list">
+//         {todo_list.map((task) => {
+//           return (
+//             <div>
+//               <h1>{task.taskname}</h1>
+//               <button onClick={()=> delete_task_func(task.id)}>x</button>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
