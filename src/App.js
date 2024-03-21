1bba368f4408b16fde98076138e0bf4be52818d6
frogshopping <amirul.alam.chowdhuryy@gmail.com>
2024-03-21 10:01:00 +0600

    k

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-// import { Todo_Comp } from './components/todoComponents/todo';
+import { Todo_Comp } from './components/todoComponents/todo';
 import {ButtonsComponent} from './components/calculatorComponents/ButtonsComponent'
 
 function App() {
   return (
     <div className="App">
-      {/* <Todo_Comp /> */}
+      <Todo_Comp />
       <ButtonsComponent/>
     </div>
   );
--- src/components/calculatorComponents/ButtonsComponent.js
+++ src/components/calculatorComponents/ButtonsComponent.js
@@ -1,43 +1,3 @@
-// import { useState } from "react";
-
-
-// export const ButtonsComponent = () => {
-//   const [input,setinput]=useState('')
-
-//   const handleButtonClick=(value)=>{
-//     setinput(input+value)
-//   }
-  
-//   return (
-//     <div className="container">
-//       <p>Calculator</p>
-//       <div></div>
-//       <div className="buttons">
-//         <input className="calc-input" value={input}></input>
-
-//         <button>C</button>
-//         <button>x</button>
-//         <button>()</button>
-//         <button>+</button>
-
-//         <button onClick={()=>handleButtonClick('7')}>7</button>
-//         <button>8</button>
-//         <button>9</button>
-//         <button>-</button>
-
-//         <button>4</button>
-//         <button>5</button>
-//         <button>6</button>
-//         <button>*</button>
-
-//         <button>.</button>
-//         <button>0</button>
-//         <button>%</button>
-//         <button>=</button>
-//       </div>
-//     </div>
-//   );
-// };
 
 import React, { useState } from "react";
 import * as math from "mathjs"; // Import math.js library
@@ -70,24 +30,19 @@ export const ButtonsComponent = () => {
 
         <button onClick={() => handleClear()}>C</button>
         <button onClick={() => handleButtonClick("*")}>x</button>
-        <button onClick={() => handleButtonClick("(")}>(</button>
+        <button onClick={() => handleButtonClick("(")}>()</button>
         <button onClick={() => handleButtonClick("+")}>+</button>
-
         <button onClick={() => handleButtonClick("7")}>7</button>
         <button onClick={() => handleButtonClick("8")}>8</button>
         <button onClick={() => handleButtonClick("9")}>9</button>
         <button onClick={() => handleButtonClick("-")}>-</button>
-
         <button onClick={() => handleButtonClick("4")}>4</button>
         <button onClick={() => handleButtonClick("5")}>5</button>
         <button onClick={() => handleButtonClick("6")}>6</button>
         <button onClick={() => handleButtonClick("*")}>*</button>
-
         <button onClick={() => handleButtonClick("3")}>3</button>
         <button onClick={() => handleButtonClick("2")}>2</button>
         <button onClick={() => handleButtonClick("1")}>1</button>
-        
-
         <button onClick={() => handleButtonClick(".")}>.</button>
         <button onClick={() => handleButtonClick("0")}>0</button>
         <button onClick={() => handleButtonClick("%")}>%</button>
